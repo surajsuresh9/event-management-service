2fefae4fe3b6fc789fc6c3a8c14aa7c82b0ec845
surajsuresh9 <49052225+surajsuresh9@users.noreply.github.com>
2025-02-04 09:31:52 +0530

    Add assertions to tests

--- src/test/java/org/event_booking_app/controller/EventControllerTest.java
+++ src/test/java/org/event_booking_app/controller/EventControllerTest.java
@@ -48,7 +48,7 @@ class EventControllerTest {
         event3 = new Event(3L, "DEF Event", "Formalin Works", LocalDateTime.of(2025, Month.JANUARY, 1, 12, 0).toString(), LocalDateTime.of(2025, Month.FEBRUARY, 1, 12, 0).toString());
         eventRequest = new EventRequest("TechnoGig", "ABC Events", LocalDateTime.now().toString(), LocalDateTime.now().plusDays(3).toString());
         objectMapper = new ObjectMapper();
-        objectMapper.findAndRegisterModules();
+//        objectMapper.findAndRegisterModules();
     }
 
     @AfterEach
@@ -66,54 +66,71 @@ class EventControllerTest {
                 .andReturn();
         List<Event> events = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {
         });
-
         Assertions.assertEquals(3, events.size());
-
     }
 
     @Test
     void getEventById() throws Exception {
         when(eventService.getEventById(any())).thenReturn(event1);
-        mockMvc.perform(get("/api/event/1"))
+        MvcResult mvcResult = mockMvc.perform(get("/api/event/1"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+        Event event = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Event.class);
+        Assertions.assertNotNull(event);
+        Assertions.assertEquals(1, event.getId());
+
     }
 
     @Test
     void getEventByStartDate() throws Exception {
         when(eventService.getEventByStartDate(any(), any())).thenReturn(Arrays.asList(event1, event2, event3));
-        mockMvc.perform(get("/api/event/date?from=2025-01-01&to=2025-10-10"))
+        MvcResult mvcResult = mockMvc.perform(get("/api/event/date?from=2025-01-01&to=2025-10-10"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+        List<Event> events = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+        Assertions.assertNotNull(events);
     }
 
     @Test
     void saveEvent() throws Exception {
         when(eventService.saveEvent(any())).thenReturn(event2);
-        mockMvc.perform(post("/api/event/")
+        MvcResult mvcResult = mockMvc.perform(post("/api/event/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(eventRequest)))
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andReturn();
+        Event event = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Event.class);
+        Assertions.assertNotNull(event);
     }
 
     @Test
     void updateEvent() throws Exception {
         when(eventService.updateEvent(any())).thenReturn(event3);
-        mockMvc.perform(put("/api/event/")
+        MvcResult mvcResult = mockMvc.perform(put("/api/event/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(event2)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        Event event = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Event.class);
+        Assertions.assertNotNull(event);
     }
 
     @Test
     void deleteEvent() throws Exception {
         when(eventService.deleteEvent(any())).thenReturn("Event deleted successfully");
-        mockMvc.perform(delete("/api/event/1"))
+        MvcResult mvcResult = mockMvc.perform(delete("/api/event/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+        String msg = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(msg);
+        Assertions.assertEquals("Event deleted successfully", msg);
     }
 }
